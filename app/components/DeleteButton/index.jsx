552d2c010cc0bf367d80ab5d9452d34da0f3648d
Djessy Coiffe <djessy.coiffe@gmail.com>
2023-12-07 21:08:28 +0100

    vercel settings

--- app/components/DeleteButton/index.jsx
+++ app/components/DeleteButton/index.jsx
@@ -20,7 +20,9 @@ export default function DeleteButton({ recipeId }) {
 
   return (
     <div>
-      <button onClick={handleClick} className="bg-red-400 text-white p-4 rounded">Delete Recipe</button>
+      <div className="flex justify-center">
+        <button onClick={handleClick} className="bg-red-400 text-white p-4 rounded">Delete Recipe</button>
+      </div>
     </div>
   )
 }
