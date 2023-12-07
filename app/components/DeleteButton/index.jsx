c117fc816a66db07fa7a7dffbeb074e80577cccc
Djessy Coiffe <djessy.coiffe@gmail.com>
2023-12-07 21:06:08 +0100

    test build

--- app/components/DeleteButton/index.jsx
+++ app/components/DeleteButton/index.jsx
@@ -1,24 +1,26 @@
 'use client'
 import { useRouter } from "next/navigation"
 
-export default function DeleteButton({recipeId}){
-    const router = useRouter()
+export default function DeleteButton({ recipeId }) {
+  const router = useRouter()
 
-    async function handleClick(){
+  async function handleClick() {
 
-        try {
-            await fetch(`/api/delete_recipe/${parseInt(recipeId)}`, {
-                method: 'DELETE'
-            })
-            router.push('/')
-            router.refresh()
-        } catch(e){
-            console.error(e)
-        }
-
+    try {
+      await fetch(`/api/delete_recipe/${parseInt(recipeId)}`, {
+        method: 'DELETE'
+      })
+      router.push('/')
+      router.refresh()
+    } catch (e) {
+      console.error(e)
     }
 
-    return (
-        <button onClick={handleClick} className="bg-red-400 text-white p-4 rounded">Delete Recipe</button>
-    )
+  }
+
+  return (
+    <div>
+      <button onClick={handleClick} className="bg-red-400 text-white p-4 rounded">Delete Recipe</button>
+    </div>
+  )
 }
