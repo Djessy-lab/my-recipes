ca3423046fa7fab1b36578e6d7ca6498914c2648
Djessy Coiffe <djessy.coiffe@gmail.com>
2023-12-22 09:00:31 +0100

    test margin

--- app/components/Accueil/index.jsx
+++ app/components/Accueil/index.jsx
@@ -7,7 +7,7 @@ const Accueil = () => {
     <GlobalLayout>
         <div>
           <div className="text-center font-extrabold text-transparent lg:text-8xl bg-clip-text bg-gradient-to-r from-sky-400 to-emerald-600 p-10">MY RECIPES</div>
-          <div className='mt-20'>
+          <div className='mt-28'>
             <Description />
           </div>
           <div className='mt-20 mb-32'>
