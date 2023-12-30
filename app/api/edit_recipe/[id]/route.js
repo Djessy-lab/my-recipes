39fe091ed2ead6bad4b53dd3d942b889a17a1cc3
Djessy Coiffe <djessy.coiffe@gmail.com>
2023-12-30 06:19:17 +0100

    new methods

--- app/api/edit_recipe/[id]/route.js
+++ app/api/edit_recipe/[id]/route.js
@@ -1,46 +1,25 @@
 import prisma from "../../../lib/prisma";
-import { getSession } from 'next-auth/react';
 
-export async function PUT(request, { params }) {
+export async function PUT(request, {params}) {
   const id = params.id;
-  const { ...recipeData } = await request.json();
-  const session = await getSession({ req: request });
+  const { ...recipe } = await request.json();
 
   if (!id) {
-    return new Response('Invalid ID', { status: 400 });
-  }
-
-  if (!session?.user?.id) {
-    return new Response('User not authenticated', { status: 401 });
+      return new Response('Invalid ID', { status: 400 });
   }
 
   try {
-    const existingRecipe = await prisma.recipes.findUnique({
-      where: {
-        id: Number(id),
-      },
-      select: {
-        userId: true,
-      },
-    });
-
-    // Vérifie si la recette existe et si elle appartient à l'utilisateur connecté
-    if (!existingRecipe || existingRecipe.userId !== session.user.id) {
-      return new Response('Recipe not found or unauthorized', { status: 403 });
-    }
-
-    const updatedRecipe = await prisma.recipes.update({
-      where: {
-        id: Number(id),
-      },
-      data: {
-        ...recipeData,
-      },
-    });
-
-    return new Response(JSON.stringify(updatedRecipe), { status: 200 });
+      const updatedRecipe = await prisma.recipes.update({
+          where: {
+             id: Number(id),
+            },
+            data: {
+              ...recipe,
+            },
+      });
+      return new Response(JSON.stringify(updatedRecipe), { status: 200 });
   } catch (error) {
-    console.error('Error updating recipe:', error);
-    return new Response('Error updating recipe', { status: 500 });
+      console.error('Error updating recipe:', error);
+      return new Response('Error updating recipe', { status: 500 });
   }
 }
--- app/api/post_recipes/route.js
+++ app/api/post_recipes/route.js
@@ -1,37 +1,17 @@
 import prisma from "../../lib/prisma";
-import { getSession } from 'next-auth/react';
-import { NextResponse } from "next/server";
+import { NextResponse } from "next/server"
 
 export async function POST(request){
-    const session = await getSession({ req: request });
+    const res = await request.json()
+    const {title, image, ingredients, instructions} = res;
+     const result = await prisma.recipes.create({
+        data: {
+            title,
+            image,
+            ingredients,
+            instructions
+        }
+     })
 
-    // Vérifie si l'utilisateur est connecté
-    if (!session?.user?.id) {
-        return NextResponse.error(new Error('User not authenticated'), { status: 401 });
-    }
-
-    try {
-        const res = await request.json();
-        const { title, image, ingredients, instructions } = res;
-
-        // Crée la recette et associe l'ID de l'utilisateur
-        const result = await prisma.recipes.create({
-            data: {
-                title,
-                image,
-                ingredients,
-                instructions,
-                user: {
-                    connect: {
-                        id: session.user.id // Associe la recette à l'utilisateur connecté
-                    }
-                }
-            }
-        });
-
-        return NextResponse.json({ result });
-    } catch (error) {
-        console.error('Error creating recipe:', error);
-        return NextResponse.error(new Error('Failed to create recipe'), { status: 500 });
-    }
+    return NextResponse.json({result})
 }
